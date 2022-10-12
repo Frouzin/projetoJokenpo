8487707817bce54c84b4dbb46380f9abaefa2b41
Frouzin <matheusereis44@gmail.com>
2022-10-11 22:58:42 -0300

    nova atualização

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ window.config(bg=fundo)
 
 # Divisão de tela
 
-frame_up=Frame(window, width=280, height=10cd0, bg=co4, relief='raised')
+frame_up=Frame(window, width=280, height=100, bg=co4, relief='raised')
 frame_up.grid(row=0, column=0, sticky=NW)
 
 frame_down = Frame(window, width=280, height=300, bg=co0, relief='flat')
@@ -31,4 +31,13 @@ frame_down.grid(row=1, column=0, sticky=NW)
 style = ttk.Style(window)
 style.theme_use('clam')
 
+# COnfigurando o Frame
+
+player_1 = Label(frame_up, text='You', height=1, anchor='center', font=('Arial 10 bold'), bg=co4, fg=co1)
+player_1.place(x=20, y=70)
+
+player_1_line = Label(frame_up, text='', height=10, anchor='center', font=('Arial 10 bold'), bg=co0, fg=co1)
+player_1_line.place(x=0, y=0)
+
+# Codigo de atualização da Tela (sempre deixar no final do código)
 window.mainloop()
