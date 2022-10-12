41bd44b216aa650752b7eda6b8e1b64a5e0d948f
Frouzin <matheusereis44@gmail.com>
2022-10-12 12:13:19 -0300

    Tela superior de pontuação atualizada!

--- main.py
+++ main.py
@@ -31,13 +31,31 @@ frame_down.grid(row=1, column=0, sticky=NW)
 style = ttk.Style(window)
 style.theme_use('clam')
 
-# COnfigurando o Frame
+# COnfigurando o Tela de Pontuação
 
 player_1 = Label(frame_up, text='You', height=1, anchor='center', font=('Arial 10 bold'), bg=co4, fg=co1)
 player_1.place(x=20, y=70)
-
 player_1_line = Label(frame_up, text='', height=10, anchor='center', font=('Arial 10 bold'), bg=co0, fg=co1)
 player_1_line.place(x=0, y=0)
+player_1_point = Label(frame_up, text='0', height=1, anchor='center', font=('Arial 30 bold'), bg=co4, fg=co1)
+player_1_point.place(x=50, y=20)
+
+Mid = Label(frame_up, text=':', height=1, anchor='center', font=('Arial 30 bold'), bg=co4, fg=co1)
+Mid.place(x=130, y=20)
+
+player_2_point = Label(frame_up, text='0', height=1, anchor='center', font=('Arial 30 bold'), bg=co4, fg=co1)
+player_2_point.place(x=200, y=20)
+player_2 = Label(frame_up, text='PC', height=1, anchor='center', font=('Arial 10 bold'), bg=co4, fg=co1)
+player_2.place(x=235, y=70)
+player_2_line = Label(frame_up, text='', height=10, anchor='center', font=('Arial 10 bold'), bg=co0, fg=co1)
+player_2_line.place(x=275, y=0)
+
+line = Label(frame_up, text='', width=275, anchor='center', font=('Arial 1 bold'), bg=co0, fg=co1)
+line.place(x=0, y=95)
+
+# Configurando tela de Game
+
+
 
 # Codigo de atualização da Tela (sempre deixar no final do código)
 window.mainloop()
